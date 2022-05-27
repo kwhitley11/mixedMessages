8fafc8990536023cd893125f75a406a924b54b68
kwhitley11 <kwhitley11@gmail.com>
2022-05-27 16:56:59 -0500

    function test

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var randomNum = Math.random(Math.floor());
+let randomNum = Math.random(Math.floor() * 3);
 console.log(randomNum)
+
+const letterkennyQuote = (randomNum) => {
+    switch (randomNum) {
+        case 1: 
+            console.log('Pitter-patter');
+            break;
+        case 2:
+            console.log('Your Sister\’s Hot, Wayne! There I Said It! I Said It! I Regret Nothing! I Regret Nothing!');
+            break;
+        case 3:
+            console.log('I\'d Say Give Yer B***S A Tug, But It Looks Like Your Pants Are Doing That For You.');
+            break;
+    }
+}
+
+letterkennyQuote(randomNum);
