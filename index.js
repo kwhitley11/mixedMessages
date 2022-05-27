dd0b1d590096b367fcdeeded26a5cf19abc66d83
kwhitley11 <kwhitley11@gmail.com>
2022-05-27 17:10:51 -0500

    letterkenny quote generator

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-let randomNum = Math.random(Math.floor() * 3);
-console.log(randomNum)
+// generates random number between 0 and 9 for function below
+let randomNum = Math.floor(Math.random() * 10);
+// console.log(randomNum);
 
+
+// randomly selects letterkenny quote with variable above
 const letterkennyQuote = (randomNum) => {
     switch (randomNum) {
-        case 1: 
+        case 0: 
             console.log('Pitter-patter');
             break;
+        case 1:
+            console.log('Your sister\’s hot, Wayne! There I said it! I said it! I regret nothing! I regret nothing!');
+            break;
         case 2:
-            console.log('Your Sister\’s Hot, Wayne! There I Said It! I Said It! I Regret Nothing! I Regret Nothing!');
+            console.log('I\'d say give yer balls a tug, but it looks like your pants are doing that for you.');
             break;
         case 3:
-            console.log('I\'d Say Give Yer B***S A Tug, But It Looks Like Your Pants Are Doing That For You.');
+            console.log('You\'re fucking ten-ply, bud');
+            break;
+        case 4:
+            console.log('If you got a problem with Canada gooses, then you got a problem with me and I suggest you let that one marinate.');
+            break;
+        case 5:
+            console.log('I see the muscle shirt came today. Muscles coming tomorrow?');
+            break;
+        case 6:
+            console.log('Wish you weren\'t so fuckin awkward, bud.');
+            break;
+        case 7:
+            console.log('Figure it out.');
+            break;
+        case 8:
+            console.log('To be faiiiiiiiiir.');
+            break;
+        case 9:
+            console.log('Why don\'t you take about 20% of \'er there, Squirrely Dan.');
             break;
     }
 }
